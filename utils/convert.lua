87eba5939ddc34a5a9d80ab0c0307e43ad1a2fc0
Andreas Hartmann <hartan@7x.de>
2023-06-18 10:23:00 +0200

    utils/convert: Embed coloring into icons
    rather than adding a new metadata field. This approach has the great
    advantage that it reduces complexity and doesn't require extra
    configuration in the users side.
    
    Thanks to @sayanarijit for the suggestion!

--- utils/convert.lua
+++ utils/convert.lua
@@ -1,19 +1,21 @@
 mod = require("raw-devicons")
-outfile = io.open("web-devicons.lua", "w")
+outfile = io.open("icons.lua", "w")
 
+outfile:write("-- this file is generated automatically, don't edit it by hand\n")
 outfile:write("local extension_map = {}\n")
 outfile:write("local special_map = {}\n\n")
 
 for key, val in pairs(mod.by_name) do
     outfile:write("special_map[\"" .. key .. "\"] = {\n")
     outfile:write("    meta = {\n")
-    outfile:write("        icon = \"" .. val.icon .. "\",\n")
-    if not (val.cterm_color == nil) then
-      outfile:write("        icon_style = '{ \"fg\": { \"Indexed\": " ..
-        val.cterm_color .. " }}',\n")
-      --outfile:write("    icon_style = {\n")
-      --outfile:write("      fg = { Indexed = " .. val.cterm_color .. " },\n")
-      --outfile:write("    }\n")
+    if val.cterm_color == nil then
+        outfile:write("        icon = \"" .. val.icon .. "\",\n")
+    else
+        outfile:write("        icon = xplr.util.paint(\""
+          .. val.icon 
+          .. "\", { fg = { Indexed = " 
+          .. val.cterm_color 
+          .. " }}),\n")
     end
     outfile:write("    }\n")
     outfile:write("}\n")
@@ -22,19 +24,18 @@ end
 for key, val in pairs(mod.by_ext) do
     outfile:write("extension_map[\"" .. key .. "\"] = {\n")
     outfile:write("    meta = {\n")
-    outfile:write("        icon = \"" .. val.icon .. "\",\n")
-    if not (val.cterm_color == nil) then
-      outfile:write("        icon_style = '{ \"fg\": { \"Indexed\": " ..
-        val.cterm_color .. " }}',\n")
-      --outfile:write("    icon_style = {\n")
-      --outfile:write("      fg = { Indexed = " .. val.cterm_color .. " },\n")
-      --outfile:write("    }\n")
+    if val.cterm_color == nil then
+        outfile:write("        icon = \"" .. val.icon .. "\",\n")
+    else
+        outfile:write("        icon = xplr.util.paint(\""
+          .. val.icon 
+          .. "\", { fg = { Indexed = " 
+          .. val.cterm_color 
+          .. " }}),\n")
     end
     outfile:write("    }\n")
     outfile:write("}\n")
 end
 
-outfile:write("\nreturn {\n")
-outfile:write("    special_map = special_map,\n")
-outfile:write("    extension_map = extension_map,\n")
-outfile:write("}")
+outfile:write("\n\n")
+outfile:close()
